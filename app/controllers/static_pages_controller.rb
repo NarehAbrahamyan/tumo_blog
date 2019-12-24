01ae575d28ea65d8234d576a795726d2eecf179b
Your Name <you@example.com>
2019-12-24 16:50:55 +0400

    navbar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,21 @@
 class StaticPagesController < ApplicationController
   def home
+    @direction=[
+    'Animation',
+    'Game Development',
+    'Filmmaking',
+    'Web Development',
+    'Music',
+    'Writing',
+    'Drawing',
+    'Graphic Design',
+    '3D Modeling',
+    'Programming',
+    'Robotics',
+    'Motion Graphics',
+    'Photography',
+    'New Media'
+  ]
   end
 
   def help
