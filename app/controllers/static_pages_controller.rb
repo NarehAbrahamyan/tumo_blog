a2ee4f159937abec9b726f1239e3a66e1702e263
Tigran <tigran.serobyan.y@tumo.org>
2019-12-25 16:45:06 +0400

    Fix conflicts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-<<<<<<< HEAD
-    # @new_posts = Post.
-=======
     @direction=[
     'Animation',
     'Game Development',
@@ -19,7 +16,6 @@ class StaticPagesController < ApplicationController
     'Photography',
     'New Media'
   ]
->>>>>>> 23f3ae4975d1e56c4146ff07c033bb0106e0078f
   end
 
   def help
