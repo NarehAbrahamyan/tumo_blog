957016deced9d29925ef42abefecc90855771ef1
anonymouzay <59052926+anonymouzay@users.noreply.github.com>
2019-12-25 13:24:32 +0400

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'static_pages#home'
+  root 'posts#index'
   
   get 'static_pages/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
