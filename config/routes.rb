5e02876ea3722775e411ed2e90973fc09d792103
Tigran <tigran.serobyan.y@tumo.org>
2019-12-26 18:10:23 +0400

    image

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root 'posts#index'
   
   get 'static_pages/help'
+  get 'users/:id' => 'users#show'
+
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts do
