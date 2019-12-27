909da15d535c8a177869802dba7bcdceb918f2b7
Tigran <tigran.serobyan.y@tumo.org>
2019-12-27 18:09:04 +0400

    done

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,11 @@ module UsersHelper
             image_url("avatar.png")
         end
     end
+    def currentuser_image
+        if current_user.avatar.attached?
+            current_user.avatar
+        else
+            image_url("avatar.png")
+        end 
+    end
 end
